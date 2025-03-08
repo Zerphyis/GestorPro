fe619a3e23cf23f593e6ec41fb79bd4c343b7fe1
Otavio <estudosprogotavio@gmail.com>
2025-03-08 18:49:49 -0300

    feat: adicionando Dto de resposta da entidade Funcionarios

---
+++ src/main/java/dev/Zerphyis/gestao/Entity/Data/DataResponseEmployee.java
@@ -0,0 +1,16 @@
+package dev.Zerphyis.gestao.Entity.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+public record DataResponseEmployee(Long id,
+                                   String name,
+                                   String document,
+                                   String email,
+                                   String phone,
+                                   LocalDate dateAdmission,
+                                   BigDecimal wage,
+                                   String positionName,
+                                   List<String> benefitsNames) {
+}
