7a24e9f7f8aee71268f1d3c391683b1e89edb829
Otavio <estudosprogotavio@gmail.com>
2025-03-10 16:52:43 -0300

    feat: adicionando o Dto formatação de saida para entidade PontoEletronico

---
+++ src/main/java/dev/Zerphyis/gestao/Entity/Data/EletronicPoints/DataEletronicPoinsReponseExit.java
@@ -0,0 +1,6 @@
+package dev.Zerphyis.gestao.Entity.Data.EletronicPoints;
+
+import java.time.LocalDateTime;
+
+public record DataEletronicPoinsReponseExit(String employeeName, LocalDateTime entryTime, LocalDateTime exitTime) {
+}
