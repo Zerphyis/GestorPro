0bf8b3d1108247552abacfff7fe4e426ec31732b
Otavio <estudosprogotavio@gmail.com>
2025-03-10 16:52:53 -0300

    feat: adicionando o Dto formatação de entrada para entidade PontoEletronico

---
+++ src/main/java/dev/Zerphyis/gestao/Entity/Data/EletronicPoints/DataEletronicPointsReponseEntry.java
@@ -0,0 +1,6 @@
+package dev.Zerphyis.gestao.Entity.Data.EletronicPoints;
+
+import java.time.LocalDateTime;
+
+public record DataEletronicPointsReponseEntry(String EmployeeName, LocalDateTime entry) {
+}
