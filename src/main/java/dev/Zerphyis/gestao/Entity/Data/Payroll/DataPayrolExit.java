50e9efb333a7ec37aacd087c18c2637a3d35e091
Otavio <estudosprogotavio@gmail.com>
2025-03-11 21:19:53 -0300

    Adicionando campo Data na Dto de saida

--- src/main/java/dev/Zerphyis/gestao/Entity/Data/Payroll/DataPayrolExit.java
+++ src/main/java/dev/Zerphyis/gestao/Entity/Data/Payroll/DataPayrolExit.java
@@ -2,6 +2,6 @@ package dev.Zerphyis.gestao.Entity.Data.Payroll;
 
 import java.math.BigDecimal;
 
-public record DataPayrolExit(BigDecimal discount, BigDecimal wage) {
+public record DataPayrolExit(BigDecimal baseSalary, BigDecimal discount, BigDecimal wage) {
 }
 
