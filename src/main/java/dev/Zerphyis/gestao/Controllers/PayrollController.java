9973f0fa676547cdb88d595e797b3b618092e922
Otavio <estudosprogotavio@gmail.com>
2025-03-11 21:18:57 -0300

    Alterando para trabalhar com nova Lógica de desconto

--- src/main/java/dev/Zerphyis/gestao/Controllers/PayrollController.java
+++ src/main/java/dev/Zerphyis/gestao/Controllers/PayrollController.java
@@ -9,58 +9,40 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/pagamento")
 public class PayrollController {
 
-        @Autowired
-        private ServicePayroll servicePayroll;
+    @Autowired
+    private ServicePayroll servicePayroll;
 
-        @PostMapping
-        public ResponseEntity<Payroll> createPayroll(@RequestBody Payroll payroll) {
-            Payroll createdPayroll = servicePayroll.createPayroll(payroll);
-            return ResponseEntity.status(HttpStatus.CREATED).body(createdPayroll);
-        }
-
-        @GetMapping("/{id}")
-        public ResponseEntity<Payroll> getPayrollById(@PathVariable Long id) {
-            return servicePayroll.getPayrollById(id)
-                    .map(ResponseEntity::ok)
-                    .orElseGet(() -> ResponseEntity.notFound().build());
-        }
-
-        @GetMapping
-        public ResponseEntity<List<Payroll>> getAllPayrolls() {
-            List<Payroll> payrolls = servicePayroll.getAllPayrolls();
-            return ResponseEntity.ok(payrolls);
-        }
-
-        @PutMapping("/{id}")
-        public ResponseEntity<Payroll> updatePayroll(@PathVariable("id") Long id, @RequestBody Payroll payroll) {
-            Payroll updatedPayroll = servicePayroll.updatePayroll(id, payroll);
-            return updatedPayroll != null ? ResponseEntity.ok(updatedPayroll) : ResponseEntity.notFound().build();
-        }
-
-        @DeleteMapping("/{id}")
-        public ResponseEntity<Void> deletePayroll(@PathVariable("id") Long id) {
-            return servicePayroll.deletePayroll(id) ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
-        }
-
-        @PostMapping("/calcular")
-        public ResponseEntity<DataPayrolExit> calculateEmployeeWage(@RequestBody DataPayrolEntry entry) {
-            BigDecimal wage = servicePayroll.calculateEmployeeWage(entry.employeeId());
-            Optional<Payroll> payroll = servicePayroll.getPayrollById(entry.employeeId());
-
-            if (payroll.isPresent()) {
-                DataPayrolExit response = new DataPayrolExit(payroll.get().getDiscount(), wage);
-                return ResponseEntity.ok(response);
-            }
-            return ResponseEntity.notFound().build();
-        }
+    @PostMapping
+    public ResponseEntity<DataPayrolExit> createPayroll(@RequestBody DataPayrolEntry entry) {
+        Payroll payroll = servicePayroll.createPayroll(entry);
+        DataPayrolExit responseDTO = new DataPayrolExit(payroll.getBaseSalary(), payroll.getDiscount(), payroll.getWage());
+        return new ResponseEntity<>(responseDTO, HttpStatus.CREATED);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DataPayrolExit> getPayroll(@PathVariable Long id) {
+        Payroll payroll = servicePayroll.getPayroll(id);
+        DataPayrolExit responseDTO = new DataPayrolExit(payroll.getBaseSalary(), payroll.getDiscount(), payroll.getWage());
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<DataPayrolExit> updatePayroll(@PathVariable Long id, @RequestBody DataPayrolEntry entry) {
+        Payroll payroll = servicePayroll.updatePayroll(id, entry);
+        DataPayrolExit responseDTO = new DataPayrolExit(payroll.getBaseSalary(), payroll.getDiscount(), payroll.getWage());
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePayroll(@PathVariable Long id) {
+        servicePayroll.deletePayroll(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
 
 
--- src/main/java/dev/Zerphyis/gestao/Service/ServicePayroll.java
+++ src/main/java/dev/Zerphyis/gestao/Service/ServicePayroll.java
@@ -1,5 +1,7 @@
 package dev.Zerphyis.gestao.Service;
 
+import dev.Zerphyis.gestao.Entity.Benefit.Benefit;
+import dev.Zerphyis.gestao.Entity.Data.Payroll.DataPayrolEntry;
 import dev.Zerphyis.gestao.Entity.Employee.Employee;
 import dev.Zerphyis.gestao.Entity.Payroll.Payroll;
 import dev.Zerphyis.gestao.Repositorys.RepositoryEmployee;
@@ -9,59 +11,80 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class ServicePayroll {
-        @Autowired
-        private RepositoryPayroll payrollRepository;
+    @Autowired
+    private RepositoryPayroll repositoryPayroll;
 
-        @Autowired
-        private RepositoryEmployee employeeRepository;
+    @Autowired
+    private RepositoryEmployee repositoryEmployee;
 
-        public Payroll createPayroll(Payroll payroll) {
-            payroll.calculateWage();
-            return payrollRepository.save(payroll);
-        }
+    public Payroll createPayroll(DataPayrolEntry entry) {
+        Long employeeId = entry.employeeId();
 
-        public Optional<Payroll> getPayrollById(Long id) {
-            return payrollRepository.findById(id);
-        }
+        Employee employee = repositoryEmployee.findById(employeeId)
+                .orElseThrow(() -> new RuntimeException("Funcionário não encontrado"));
 
-        public List<Payroll> getAllPayrolls() {
-            return payrollRepository.findAll();
-        }
+        List<Benefit> benefits = employee.getBenefits();
 
-        public Payroll updatePayroll(Long id, Payroll updatedPayroll) {
-            if (payrollRepository.existsById(id)) {
-                updatedPayroll.calculateWage();
-                return payrollRepository.save(updatedPayroll);
-            }
-            return null;
-        }
+        BigDecimal totalBenefits = benefits.stream()
+                .map(Benefit::getBenefitValue)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
 
-        public boolean deletePayroll(Long id) {
-            if (payrollRepository.existsById(id)) {
-                payrollRepository.deleteById(id);
-                return true;
-            }
-            return false;
-        }
+        BigDecimal discount = calculateDiscount(totalBenefits, benefits.size());
 
-        public BigDecimal calculateEmployeeWage(Long employeeId) {
-            Optional<Employee> employee = employeeRepository.findById(employeeId);
+        BigDecimal finalWage = employee.getWage().add(totalBenefits).subtract(discount);
 
-            if (employee.isPresent()) {
-                Payroll payroll = payrollRepository.findByEmployeeId(employeeId);
-
-                if (payroll != null) {
-                    payroll.calculateWage();
-                    payrollRepository.save(payroll);
-                    return payroll.getWage();
-                }
-            }
-            return BigDecimal.ZERO;
-        }
+        Payroll payroll = new Payroll(employee, employee.getWage(), discount, benefits);
+        payroll.setWage(finalWage);
+        return repositoryPayroll.save(payroll);
     }
+
+    public Payroll getPayroll(Long id) {
+        return repositoryPayroll.findById(id)
+                .orElseThrow(() -> new RuntimeException("Folha de pagamento não encontrada"));
+    }
+
+    public Payroll updatePayroll(Long id, DataPayrolEntry entry) {
+        Payroll payroll = repositoryPayroll.findById(id)
+                .orElseThrow(() -> new RuntimeException("Folha de pagamento não encontrada"));
+
+        Long employeeId = entry.employeeId();
+
+        Employee employee = repositoryEmployee.findById(employeeId)
+                .orElseThrow(() -> new RuntimeException("Funcionário não encontrado"));
+
+        List<Benefit> benefits = employee.getBenefits();
+
+        BigDecimal totalBenefits = benefits.stream()
+                .map(Benefit::getBenefitValue)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        BigDecimal discount = calculateDiscount(totalBenefits, benefits.size());
+
+        BigDecimal finalWage = employee.getWage().add(totalBenefits).subtract(discount);
+
+        payroll.setBaseSalary(employee.getWage());
+        payroll.setDiscount(discount);
+        payroll.setBenefit(benefits);
+        payroll.setWage(finalWage);
+
+        return repositoryPayroll.save(payroll);
+    }
+
+    public void deletePayroll(Long id) {
+        Payroll payroll = repositoryPayroll.findById(id)
+                .orElseThrow(() -> new RuntimeException("Folha de pagamento não encontrada"));
+
+        repositoryPayroll.delete(payroll);
+    }
+
+    private BigDecimal calculateDiscount(BigDecimal totalBenefits, int benefitsCount) {
+        BigDecimal discountPercentage = new BigDecimal("0.04").multiply(new BigDecimal(benefitsCount));
+        return totalBenefits.multiply(discountPercentage);
+    }
+}
+
 
 
