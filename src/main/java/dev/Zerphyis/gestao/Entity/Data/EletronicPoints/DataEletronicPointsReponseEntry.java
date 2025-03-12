017e7acf3d80e5c2b823ca248c8f92a0983a502f
Otavio <estudosprogotavio@gmail.com>
2025-03-11 21:19:42 -0300

    Adicionando campo Data na Dto de entrada

--- src/main/java/dev/Zerphyis/gestao/Entity/Data/EletronicPoints/DataEletronicPointsReponseEntry.java
+++ src/main/java/dev/Zerphyis/gestao/Entity/Data/EletronicPoints/DataEletronicPointsReponseEntry.java
@@ -1,6 +1,8 @@
 package dev.Zerphyis.gestao.Entity.Data.EletronicPoints;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
-public record DataEletronicPointsReponseEntry(String EmployeeName, LocalDateTime entry) {
+public record DataEletronicPointsReponseEntry(String EmployeeName, LocalTime entry, LocalDate date) {
 }
