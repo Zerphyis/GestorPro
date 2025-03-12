acb28b620b3e37b23e15850466ed030372c7a911
Otavio <estudosprogotavio@gmail.com>
2025-03-12 14:12:55 -0300

    alterando a controller da entidade Pagamento

--- src/main/java/dev/Zerphyis/gestao/Controllers/PayrollController.java
+++ src/main/java/dev/Zerphyis/gestao/Controllers/PayrollController.java
@@ -3,11 +3,14 @@ package dev.Zerphyis.gestao.Controllers;
 import dev.Zerphyis.gestao.Entity.Data.Payroll.DataPayrolEntry;
 import dev.Zerphyis.gestao.Entity.Data.Payroll.DataPayrolExit;
 import dev.Zerphyis.gestao.Entity.Payroll.Payroll;
+import dev.Zerphyis.gestao.Entity.Position.Position;
 import dev.Zerphyis.gestao.Service.ServicePayroll;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -24,22 +27,28 @@ public class PayrollController {
         return new ResponseEntity<>(responseDTO, HttpStatus.CREATED);
     }
 
+    @GetMapping()
+    public ResponseEntity<List<DataPayrolExit>> ListAllPayroll() {
+        List<DataPayrolExit> listt = servicePayroll.ListAll();
+        return ResponseEntity.ok(listt);
+    }
+
     @GetMapping("/{id}")
-    public ResponseEntity<DataPayrolExit> getPayroll(@PathVariable Long id) {
+    public ResponseEntity<DataPayrolExit> getPayroll(@PathVariable("id") Long id) {
         Payroll payroll = servicePayroll.getPayroll(id);
         DataPayrolExit responseDTO = new DataPayrolExit(payroll.getBaseSalary(), payroll.getDiscount(), payroll.getWage());
         return new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<DataPayrolExit> updatePayroll(@PathVariable Long id, @RequestBody DataPayrolEntry entry) {
+    public ResponseEntity<DataPayrolExit> updatePayroll(@PathVariable("id") Long id, @RequestBody DataPayrolEntry entry) {
         Payroll payroll = servicePayroll.updatePayroll(id, entry);
         DataPayrolExit responseDTO = new DataPayrolExit(payroll.getBaseSalary(), payroll.getDiscount(), payroll.getWage());
         return new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deletePayroll(@PathVariable Long id) {
+    public ResponseEntity<Void> deletePayroll(@PathVariable("id") Long id) {
         servicePayroll.deletePayroll(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
