b9d0af3184962a85ec8c71e5e4aca2755191d46b
Otavio <estudosprogotavio@gmail.com>
2025-03-17 14:43:02 -0300

    Adicionando Service do token da aplicação

---
+++ src/main/java/dev/Zerphyis/gestao/Security/ServiceToken.java
@@ -0,0 +1,51 @@
+package dev.Zerphyis.gestao.Security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import dev.Zerphyis.gestao.Entity.Login.Login;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class ServiceToken {
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(Login user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("gestao")
+                    .withSubject(user.getUsername())
+                    .withClaim("role", user.getRole().name())
+                    .withExpiresAt(genExpirationDate())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Erro ao gerar token", exception);
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            DecodedJWT decodedJWT = JWT.require(algorithm)
+                    .withIssuer("gestao")
+                    .build()
+                    .verify(token);
+            return decodedJWT.getSubject();
+        } catch (JWTVerificationException exception) {
+            return null;
+        }
+    }
+
+    private Instant genExpirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
