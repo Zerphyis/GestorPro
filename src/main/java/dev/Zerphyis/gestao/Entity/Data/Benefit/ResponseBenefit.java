e2f9cca4393e974421bd650f98c3d400087fa140
Otavio <estudosprogotavio@gmail.com>
2025-03-17 14:39:58 -0300

    Adicionando Formatação de resposta de beneficios

---
+++ src/main/java/dev/Zerphyis/gestao/Entity/Data/Benefit/ResponseBenefit.java
@@ -0,0 +1,7 @@
+package dev.Zerphyis.gestao.Entity.Data.Benefit;
+
+import java.math.BigDecimal;
+
+public record ResponseBenefit(String nameBenefit,
+                              BigDecimal benefitValue) {
+}
