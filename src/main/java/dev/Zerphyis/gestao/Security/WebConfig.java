50e543ac600815a3e9b5d7d7a724e431fa76e2e7
Otavio <estudosprogotavio@gmail.com>
2025-03-17 14:43:47 -0300

    Adicionando limites das roles da aplicação

---
+++ src/main/java/dev/Zerphyis/gestao/Security/WebConfig.java
@@ -0,0 +1,50 @@
+package dev.Zerphyis.gestao.Security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@Configuration
+public class WebConfig {
+    @Autowired
+    private SecurityFilter securityFilter;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/login/auth").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/login/register").permitAll()
+                        .requestMatchers("/ponto/**").hasAuthority("ROLE_EMPLOYEE")
+                        .requestMatchers("/funcionarios/**").hasAuthority("ROLE_MANEGER")
+                        .requestMatchers("/cargos/**").hasAuthority("ROLE_FINANCE")
+                        .requestMatchers("/beneficios/**").hasAuthority("ROLE_FINANCE")
+                        .requestMatchers("/**").hasAuthority("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
